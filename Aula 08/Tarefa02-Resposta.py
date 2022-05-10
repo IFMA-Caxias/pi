81d0ad5f45d3223e9078d29d6125d8e5322a56a8
Luis Fernando Maia <luisfmss@gmail.com>
2022-05-10 07:58:22 -0300

    Correção atividade Aula 08

--- Aula 08/Tarefa02-Resposta.py
+++ Aula 08/Tarefa02-Resposta.py
@@ -2,11 +2,11 @@ from email.mime import base
 import cv2
 import numpy as np
 
-img = cv2.imread('cars.png', 3)
-mask = np.ones(img.shape, np.uint8)
+img = cv2.imread('cars.png')
+mask = np.zeros(img.shape, np.uint8)
 cv2.circle(mask, (mask.shape[1] - 480, mask.shape[0] -
            300), 270, (255, 255, 255), -1)
-mask = cv2.GaussianBlur(mask, (51,51), 51)
+mask = cv2.GaussianBlur(mask, (151,151), 151)
 
 maskInv = cv2.bitwise_not(mask)
 imgBlur = cv2.blur(img, (50, 50))
@@ -18,11 +18,12 @@ imageMaskedBack=imageMaskedBack.astype(np.uint8)
 imageMaskedFocus = img * (mask / 255)
 imageMaskedFocus=imageMaskedFocus.astype(np.uint8)
 
-res = cv2.add(imageMaskedBack, imageMaskedFocus,  dtype=cv2.CV_8U)
+res = cv2.add(imageMaskedBack, imageMaskedFocus)
 
 cv2.imshow('noBlur', img)
 cv2.imshow('blur', imgBlur)
-cv2.imshow('mask', maskInv)
+cv2.imshow('mask', mask)
+cv2.imshow('maskInv', maskInv)
 cv2.imshow('imageMaskedBack', imageMaskedBack)
 cv2.imshow('imageMaskedFocus', imageMaskedFocus)
 cv2.imshow('res', res)
